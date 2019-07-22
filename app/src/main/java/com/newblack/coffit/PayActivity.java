95db79cd0f76fb38bd73ef4e652188a100bf945e
jisoo lee <jisoolee.dev@gmail.com>
2019-07-22 23:30:06 +0900

    payActivity added

--- app/src/main/java/com/newblack/coffit/PayActivity.java
+++ app/src/main/java/com/newblack/coffit/PayActivity.java
@@ -1,14 +1,40 @@
 package com.newblack.coffit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class PayActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    TextView tv_count;
+    int ptNum = 4;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pay);
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("PT 결제");
+        tv_count = findViewById(R.id.tv_count);
+        tv_count.setText(ptNum);
+
+    }
+
+    public void clickPay(View view){
+        //결제화면 나중에 붙이기
+    }
+
+    public void plus(View view){
+        ptNum++;
+        tv_count.setText(ptNum);
+    }
+
+    public void minus(View view){
+        ptNum--;
+        tv_count.setText(ptNum);
     }
 }
